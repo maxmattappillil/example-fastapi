ac346ae049fa0c9124e1ba5c3c855e0222ca83e0
Max Mattappillil <max.mattappillil@gmail.com>
2024-06-03 15:16:32 -0400

    set POSTGRES_DB env variable before running pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ from app.database import get_db, Base
 
 from app.oauth2 import create_access_token
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 # Responsible for establishing the connection to the database
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
