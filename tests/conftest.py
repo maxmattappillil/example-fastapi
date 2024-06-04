2093e495640e760df88b1bb8d712b225661efc99
Max Mattappillil <max.mattappillil@gmail.com>
2024-06-04 13:38:04 -0400

    Added some formatting to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,10 +14,16 @@ from app.database import get_db, Base
 
 from app.oauth2 import create_access_token
 
+#Use this for GitHub Actions
+#===========================================================
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+#===========================================================
 
 #Use this for local development
+#===========================================================
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+#===========================================================
+
 
 # Responsible for establishing the connection to the database
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
